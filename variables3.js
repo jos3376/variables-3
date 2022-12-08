61b0b071bb65bfe125b4bfcb2594ca25d9b7548c
jose ismael rodriguez <jr3376@hotmail.com>
2022-12-08 10:26:45 -0300

    variables 3

---
+++ variables3.js
@@ -0,0 +1,34 @@
+
+let diaSemana = prompt("¿Qué día es hoy?");
+switch (diaSemana) {
+case "lunes":
+alert("Hoy 1 hora de caminata");
+break;
+case "martes":
+alert("Hoy 1 hora de bicicleta");
+break;
+case "miercoles":
+alert("Hoy 1/2 hora de trote");
+break;
+case "jueves":
+alert("Hoy 15 minutos de soga");
+break;
+case "viernes":
+alert("Hoy gimnasio");
+break;
+case "sábado":
+alert("Hoy 1 hora de cicicleta");
+break;
+case "domingo":
+alert("Hoy dia libre");
+break;
+default:
+alert("Escribe el día de la semana en minúsculas.");
+}
+for(i=20;i<=70;i++){
+document.write("El número es: " + i + "<br>");
+}
+const Numero = prompt('Escribe un número')
+for (resultado = 0; resultado < Numero ; resultado++) [
+document.write(" hola mundo ")
+]
